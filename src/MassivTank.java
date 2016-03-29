2f627b1d27ff3be3c819cf05a3344f8cefaf8245
Sernikoff <sernik@i.ua>
2016-03-29 12:50:27 +0300

    jhjhjhj

--- src/MassivTank.java
+++ src/MassivTank.java
@@ -1,5 +1,5 @@
 //
-//public class MassivTank {
+public class MassivTank {
 //
 //	public static void main(String[] args) {
 //		Tank [] tank = new Tank[5];
@@ -11,4 +11,4 @@
 //	static  void printTankInfo(Tank[] tank){
 //		System.out.println(tank[1].color);
 //	}
-//}
+}
